a099a88395b51b91b3ac6f5456e047e250ad9cf3
tovehydmark <tovehydmark@gmail.com>
2022-02-17 19:52:50 +0100

    animal html and added service

--- src/app/services/animal-storage.service.ts
+++ src/app/services/animal-storage.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Animal } from '../modules/Animal';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimalStorageService {
+  private animalList: Animal[] = [];
+  animalList$: Observable<Animal[]> = of(this.animalList);
 
-  constructor() { }
+  constructor() {}
+
+  addAnimal(newAnimalFromUser: Animal) {
+    this.animalList.push(newAnimalFromUser);
+  }
 }
