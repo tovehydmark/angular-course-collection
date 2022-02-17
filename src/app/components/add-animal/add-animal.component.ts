73e5a3229030c840a761db1a5b444dbb0bc5e192
tovehydmark <tovehydmark@gmail.com>
2022-02-17 19:20:38 +0100

    added add-animal html and functionality in ts file

--- src/app/components/add-animal/add-animal.component.ts
+++ src/app/components/add-animal/add-animal.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/modules/Animal';
+import { AnimalStorageService } from 'src/app/services/animal-storage.service';
 
 @Component({
   selector: 'app-add-animal',
   templateUrl: './add-animal.component.html',
-  styleUrls: ['./add-animal.component.scss']
+  styleUrls: ['./add-animal.component.scss'],
 })
 export class AddAnimalComponent implements OnInit {
+  newAnimalFromUser: Animal = new Animal('', '', '', 0);
 
-  constructor() { }
+  constructor(private service: AnimalStorageService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveAnimal(species: string, name: string, diet: string, weight: number) {
+    this.newAnimalFromUser = new Animal(species, name, diet, weight);
+    this.service.addAnimal(this.newAnimalFromUser);
   }
-
 }
